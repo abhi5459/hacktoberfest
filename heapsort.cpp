8e9a14b08d5fed1d27505b20e68879af014427d5
Aman <amanmeel1911@gmail.com>
2020-10-06 10:55:20 +0530

    fft added

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,5 +1,49 @@
 #include <iostream> 
 using namespace std; 
+typedef complex<double> cd; 
+  
+// Recursive function of FFT 
+vector<cd> fft(vector<cd>& a) 
+{ 
+    int n = a.size(); 
+  
+    // if input contains just one element 
+    if (n == 1) 
+        return vector<cd>(1, a[0]); 
+  
+    // For storing n complex nth roots of unity 
+    vector<cd> w(n); 
+    for (int i = 0; i < n; i++) { 
+        double alpha = 2 * M_PI * i / n; 
+        w[i] = cd(cos(alpha), sin(alpha)); 
+    } 
+  
+    vector<cd> A0(n / 2), A1(n / 2); 
+    for (int i = 0; i < n / 2; i++) { 
+  
+        // even indexed coefficients 
+        A0[i] = a[i * 2]; 
+  
+        // odd indexed coefficients 
+        A1[i] = a[i * 2 + 1]; 
+    } 
+  
+    // Recursive call for even indexed coefficients 
+    vector<cd> y0 = fft(A0);  
+  
+    // Recursive call for odd indexed coefficients 
+    vector<cd> y1 = fft(A1); 
+  
+    // for storing values of y0, y1, y2, ..., yn-1. 
+    vector<cd> y(n); 
+  
+    for (int k = 0; k < n / 2; k++) { 
+        y[k] = y0[k] + w[k] * y1[k]; 
+        y[k + n / 2] = y0[k] - w[k] * y1[k]; 
+    } 
+    return y; 
+} 
+ 
 void heapify(int arr[], int n, int i) 
 { 
     int largest = i; // Initialize largest as root 
