1d27b5f1cafb97d0270480d44ca06a879890a2b8
Aman <amanmeel1911@gmail.com>
2020-10-06 11:25:29 +0530

    bfs added

--- heapsort.cpp
+++ heapsort.cpp
@@ -2,7 +2,38 @@
 using namespace std; 
 typedef complex<double> cd; 
   const int ALPHABET_SIZE = 26; 
+void edge(int a, int b) 
+{ 
+    g[a].pb(b); 
   
+    // for undirected graph add this line 
+    // g[b].pb(a); 
+} 
+  
+void bfs(int u) 
+{ 
+    queue<int> q; 
+  
+    q.push(u); 
+    v[u] = true; 
+  
+    while (!q.empty()) { 
+  
+        int f = q.front(); 
+        q.pop(); 
+  
+        cout << f << " "; 
+  
+        // Enqueue all adjacent of f and mark them visited  
+        for (auto i = g[f].begin(); i != g[f].end(); i++) { 
+            if (!v[*i]) { 
+                q.push(*i); 
+                v[*i] = true; 
+            } 
+        } 
+    } 
+} 
+    
 // trie node 
 struct TrieNode 
 { 
