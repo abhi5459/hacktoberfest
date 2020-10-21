d7777858d434aeb5bb18cb7cb802e1ac56aa2742
goku2610 <56107272+goku2610@users.noreply.github.com>
2020-10-21 20:59:35 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -213,3 +213,27 @@ int main()
   
     return 0; 
 }
+
+##c++ program to find largest three no
+
+
+#include <iostream>
+using namespace std;
+
+int main() {    
+    float n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+
+    if(n1 >= n2 && n1 >= n3)
+        cout << "Largest number: " << n1;
+
+    if(n2 >= n1 && n2 >= n3)
+        cout << "Largest number: " << n2;
+    
+    if(n3 >= n1 && n3 >= n2)
+        cout << "Largest number: " << n3;
+  
+    return 0;
+}
